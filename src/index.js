7b294e7659ef5d325a6b57dd23706fc9cc113bc4
Corey <coreyjpieper@gmail.com>
2019-05-26 12:51:55 -0500

    Basic app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//
+//   return <div>Season Display</div>;
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, error: '' };
+  }
+
+  // equivalent to above
+  state = { lat: null, error: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ error: err.message })
+    );
+    console.log("My component was rendered to the screen");
+  }
+
+  componentDidUpdate() {
+      console.log("My component was just updated - it rerendered");
+  }
+
+  // Have to define render
+  render() {
+    if (this.state.error) { // if error occurs
+      return <div>An error occured: {this.state.error}</div>;
+    } else if (!this.state.lat) { // if lat has not been updated
+      return <div> Loading... </div>;
+    } else { // no errors, lat has been updated
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
